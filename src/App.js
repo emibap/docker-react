baaa7512aadde52b6f271d37128b4f98d8faef5c
Emiliano Angelini <emiliano.angelini@whiteops.com>
2022-08-29 16:19:45 -0300

    Change feature

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Bye, loco!
-
         </p>
+        <p>
+          I was changed on a feature branch
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
